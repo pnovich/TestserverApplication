015177ba60c249b2a6a688adb2c554cc8c6168cf
unknown <pppp_84@ukr.net>
2021-03-19 02:24:48 +0200

    initial commit

---
+++ src/main/java/com/example/testserver/Event.java
@@ -0,0 +1,7 @@
+package com.example.testserver;
+
+public class Event {
+    public String title;
+    public Event(String title){this.title = title;}
+    public Event(){this.title = "title";}
+}
---
+++ src/main/java/com/example/testserver/TestController.java
@@ -0,0 +1,50 @@
+package com.example.testserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+    @Autowired
+    CustomerRepository customerRepository;
+    @CrossOrigin
+    @GetMapping("/")
+    public String test(){
+        return "test";
+    }
+
+    @GetMapping("/create")
+    public String createCustomer(){
+        customerRepository.save(new Customer("1","2"));
+        return "create";
+    }
+
+    @GetMapping("/list")
+    public List<Customer> createList(){
+        return customerRepository.findAll();
+    }
+//
+//    @PostMapping("/tutorials")
+//    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+//        try {
+//            Tutorial _tutorial = tutorialRepository
+//                    .save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+//            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+    @CrossOrigin
+    @PostMapping("/request")
+    public ResponseEntity postController(
+            @RequestBody  Event event) {
+
+//        exampleService.fakeAuthenticate(loginForm);
+        System.out.println(event);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
